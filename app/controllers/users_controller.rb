a7255ab6a16b6b1a864741417233dcdfef542608
Philip Sterling <flatironschool@Flatirons-MacBook-Air-8.local>
2019-07-08 16:51:37 -0700

    added password and fixed show slightly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show]
     def index
-        
+        @users = User.all
+    end
+    def new
+     @user = User.new
+    end
+
+    def show
+     @count = 1
+     @orders = @user.orders
+    end
+
+
+    private
+    def set_user
+     @user = User.find(params[:id])
     end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_231025) do
+ActiveRecord::Schema.define(version: 2019_07_08_235029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_07_08_231025) do
     t.string "username"
     t.integer "age"
     t.string "address"
+    t.string "password"
   end
 
 end
