d9a2c1c736b6b96f0b4b19a5038202a323b32124
Philip Sterling <flatironschool@Flatirons-MacBook-Air-8.local>
2019-07-09 11:13:33 -0700

    added drink functionality

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,2 +1,17 @@
 class DrinksController < ApplicationController
+    def add_drink
+        @drink = Drink.find(params[:id])
+        if session.include? :user_id
+            @cur_user = User.find(session[:user_id])
+        end
+        @order = @cur_user.orders.find do |order| 
+            order.complete == false
+        end
+        if !@order
+            @order = Order.new(complete: false)
+            @cur_user.orders << @order
+        end
+        @order.drinks << @drink
+        redirect_to @order
+    end
 end
