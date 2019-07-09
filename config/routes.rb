7d6f22962d65f831d633d9490d3f28ec9f3f4999
Rohit <rohitpratti@gmail.com>
2019-07-08 17:29:50 -0700

    finished crud for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :cards
+  resources :users
   root 'users#index'
   resources :drink_orders
   resources :food_orders
   resources :orders
   resources :drinks
   resources :foods
-  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
- 
