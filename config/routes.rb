f0895c3b930b527d7fc9b50b774786c4c1bbe0c6
Rohit <rohitpratti@gmail.com>
2019-07-09 10:41:13 -0700

    login page created and working with password

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
-  resources :cards
-  resources :users
   root 'users#index'
   resources :drink_orders
   resources :food_orders
   resources :orders
   resources :drinks
   resources :foods
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :cards
+  resources :users
+
+
+
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
