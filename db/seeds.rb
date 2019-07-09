cabb78d160cdbdb3d1e978dd084b4be14498c58e
Rohit <rohitpratti@gmail.com>
2019-07-09 13:23:01 -0700

    seed data completed not tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,41 @@ Card.destroy_all
 User.create(name: "Rohit Pratti", username: "Marth00165", password: "abc", password_confirmation: "abc", age: 23)
 User.create(name: "Philip Sterling", username: "Zanith", password: "abc", password_confirmation: "abc", age: 24)
 
+foods =
+ [{name:"Green Curry",price:13.95,imgurl:"https://res.cloudinary.com/grubhub/image/upload/f_auto,fl_lossy,q_100,c_fill,w_376/elopc5iejzoyhuxettdt",desc:"Green curry paste with bamboo shoots, bell peppers, basil, eggplant and coconut milk. Sweeter than red curry. Does not come with rice. If you would like rice, it will be side order please. Gluten free."},
+  {name:"Red Curry",price:13.95,imgurl:"https://res.cloudinary.com/grubhub/image/upload/f_auto,fl_lossy,q_100,c_fill,w_376/tuhgl25vvgkerwhbilcj",desc:"Red curry with bamboo shoots, bell peppers, basil and coconut milk. Does not come with rice. If you would like rice, it will be side order please. Gluten free."},
+  {name:"Pahd Thai",price:12.95,imgurl:"https://res.cloudinary.com/grubhub/image/upload/f_auto,fl_lossy,q_100,c_fill,w_376/mhcqq0obj6hmsqnnzdfp",desc:"Stir fried rice noodles with egg, ground peanuts, fresh bean sprouts, green onions and carrot. Gluten free."},
+  {name:"Chicken Noodles",price:12.95,imgurl:"https://res.cloudinary.com/grubhub/image/upload/f_auto,fl_lossy,q_100,c_fill,w_376/skrqbnnln9c94lczkjyo",desc:"Stir fried wide rice noodles with chicken, egg, tomatoes, pineapples and carrots."},
+  {name:"Stir Fried Beef Salad",price:13.75,imgurl:"https://cdn-image.myrecipes.com/sites/default/files/styles/medium_2x/public/image/recipes/ay/09/beef-salad-ay-1892139-x.jpg?itok=QMx5sYf9",desc:"Stir fried beef with lettuce, onions, cucumbers, tomatoes and lime juice."},
+  {name:"Tom Yum Soup",price:11.50,imgurl:"https://res.cloudinary.com/grubhub/image/upload/f_auto,fl_lossy,q_100,c_fill,w_376/uvekllbykdmctwlj5vzp",desc:"Hot and sour soup with mushrooms, lemongrass, lime leaves and chili paste. Gluten free."},
+  {name:"Deep Fried Spring Rolls",price:7.95,imgurl:"https://res.cloudinary.com/grubhub/image/upload/f_auto,fl_lossy,q_100,c_fill,w_376/tgoosy4j5mmplkidctxb",desc:"4 pieces. Mixed vegetables wrapped in spring roll wrappers and fried."},
+  {name:"Blanket Prawns",price:9.50,imgurl:"https://res.cloudinary.com/grubhub/image/upload/f_auto,fl_lossy,q_100,c_fill,w_376/j6g1xy0yamgd9s4lzhyq",desc:"Prawns with cream cheese wrapped in spring roll wrappers and fried. Served with tangy plum sauce."},
+  {name:"Chicken Satay",price:9.75,imgurl:"https://res.cloudinary.com/grubhub/image/upload/f_auto,fl_lossy,q_100,c_fill,w_376/e1qdfnvxkcrfepe9cqct",desc:"4 skewers. Marinated in curry powder and coconut milk. Grilled and served with mild spicy peanut sauce and pickled cucumbers. Gluten free."},
+  {name:"Grilled Chicken",price:16.50,imgurl:"https://res.cloudinary.com/grubhub/image/upload/f_auto,fl_lossy,q_100,c_fill,w_376/vdgbtb7abwbmny8sk3qv",desc:"A Thai spin on an Indian favorite. Tender chicken marinated in a sweet and spicy mixture of coconut milk, yellow curry and other flavorful spices and then grilled. Does not come with rice. If you would like rice, it will be side order please. Gluten free."}
+]
+
+foods.each do |food| {
+  Food.create(food)
+}
+end
+drinks = [
+  {name:"Coke",price:1.50,imgurl:"https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSWMLHvz3tfoG32TxdmJ7hWPzWH17F2DOd_Hggk2joIhp8vbn9l5QPE64W0Rv-l7wPesPs6IidsaEmur0ZPGlXBErrsbX4WNw1SpxBCfxRZtBZL-SzHaMkr-g&usqp=CAc",desc:"Can of Coke soda"},
+  {name:"Long Island",price:12.00,imgurl:"http://www.thedrinkkings.com/wp-content/uploads/2014/06/Long-Island-Iced-Tea-4-630-630x867.jpg",desc:"Long Island Iced Tea"},
+  {name:"Water",price:1.00,imgurl:"http://static1.squarespace.com/static/583ca2f2d482e9bbbef7dad9/58863361d2b857a1707bf488/588677d403596ebc5f1d798b/1534376808162/iStock-5078775151900.jpg?format=1500w",desc:"Quench yo thirst boul"},
+  {name:"Daiquiri",price:15.00,imgurl:"https://www.thespruceeats.com/thmb/NlUr5BfsXq9uhuPne_RUJxmird8=/1365x1365/smart/filters:no_upscale()/classic-daiquiri-recipe-759294-13_preview-5b0ecc23ba6177003649f0ce.jpeg",desc:"Cocktail with cranberry juice"},
+  {name:"Orange Juice",price:5.00,imgurl:"https://assets.marthastewart.com/styles/wmax-520-highdpi/d20/oj-upgrade-103121806/oj-upgrade-103121806_horiz_0.jpg?itok=FwA6P7iy",desc:"Freshly Squeezed Orange Juice"},
+  {name:"Cranberry Juice",price:5.00,imgurl:"https://previews.123rf.com/images/margo555/margo5551207/margo555120700015/14372872-glass-of-cranberry-juice-isolated-on-white.jpg",desc:"Cranberry Juice with Ice"},
+  {name:"Ro-Special",price:6.90,imgurl:"https://i.pinimg.com/originals/9b/8c/d3/9b8cd318589a5133df256563dd9b42d8.jpg",desc:"Classic Trash Can with a Rohitwist"},
+  {name:"Philip-Drank",price:4.20,imgurl:"https://media.glamour.com/photos/5695977393ef4b09520d4b3f/master/w_1600%2Cc_limit/health-fitness-blogs-vitamin-g-1205-apple-juice_vg.jpg",desc:"Some Apple Juice"},
+  {name:"Boba Tea",price:5.00,imgurl:"http://bobachine.com/wp-content/uploads/2017/11/IMG_0102.jpg",desc:"Boba Teas So YUMMY!"},
+  {name:"Dank",price:420.00,imgurl:"https://i.ytimg.com/vi/hm27yNEMOf8/maxresdefault.jpg",desc:"DANK ASS DRINK"}
+]
+
+drinks.each do |drink|{
+  Drink.create(drink)
+}
+end
+
 3.times do
     Card.create(
       card_no: rand(1e15...1e16).to_i.to_s,
@@ -35,22 +70,8 @@ end
     )
 end
 
-10.times do
-    Food.create(
-      name: Faker::Food.dish,
-      price: rand(1..5),
-      imgurl: Faker::Food.spice,
-      desc: Faker::Food.description
-    )
-end
-10.times do
-    Drink.create(
-      name: Faker::Beer.name,
-      price: rand(1..5),
-      imgurl: Faker::Beer.yeast,
-      desc: Faker::Food.description
-    )
-end
+
+
 8.times do
     Order.create(
       user_id: User.last.id,
@@ -69,4 +90,3 @@ end
 30.times do
     Order.all.sample.foods << Food.all.sample
 end
-
